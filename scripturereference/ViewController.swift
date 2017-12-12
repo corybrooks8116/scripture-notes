daa7b3e06f05d8d24c6fa8bfaa1bdb4784d30335
Cory Gaston <coryg81@gmail.com>
2017-12-11 20:06:36 -0500

    second commit

---
+++ scripturereference/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scripturereference
+//
+//  Created by Cory Gaston on 12/11/17.
+//  Copyright © 2017 Cory Gaston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
